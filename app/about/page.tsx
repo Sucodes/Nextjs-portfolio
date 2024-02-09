e8300df678bbc88ffdce0f3262ddb8bb2a4e50f9
oghenesuvwe money <rhounamoney@gmail.com>
2024-02-09 19:23:54 +0100

    chore: about page stack update

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -11,18 +11,18 @@ const page = () => {
             Get to know me
           </h2>
           <p className="mt-4 text-zinc-400">
-            Hi again! Since you're here, I'm guessing you want to know a little
-            about me. Alright, if you insist. I'm a Software Developer
-            specializing in frontend development. I enjoy creating engaging and
-            user-friendly experiences on the web. Fluent in TypeScript and
-            React, I strive to deliver clean and accessible code while keeping
-            the end user in mind. Collaboration is where I truly shine. I thrive
-            when bouncing ideas off others and working together to solve
-            problems. And when I'm not immersed in the world of tech, you'll
-            probably find me buried in a good book of African literature or
-            glued to the screen, indulging in some sci-fi movie marathon. Oh,
-            and here's a little secret: I actually made a career switch from
-            Medicine to Software Development! But let's keep that between us 😉
+            Hi again! Thanks for stopping by. If you're curious to learn more
+            about me, well, here goes! I'm a Software Developer specializing in
+            frontend development. I enjoy creating engaging and user-friendly
+            experiences on the web. Fluent in TypeScript and React, I strive to
+            deliver clean and accessible code while keeping the end user in
+            mind. Collaboration is where I truly shine. I thrive when bouncing
+            ideas off others and working together to solve problems. And when
+            I'm not immersed in the world of tech, you'll probably find me
+            buried in African literature or indulging in some sci-fi movie
+            marathon. Oh, and here's a little secret: I actually made a career
+            switch from Medicine to Software Development! But let's keep that
+            between us 😉
           </p>
         </div>
 
@@ -34,19 +34,28 @@ const page = () => {
 
         <ul>
           <li className="mt-4 text-zinc-400 list-disc ml-4">
-            <span className="border-b">Languages and Frameworks/Tools:</span>
+            <span className="border-b">
+              Languages and Frameworks/Libraries:
+            </span>
             <ul className="list-disc mt-4">
               <span className="mt-4">
-                TypeScript, JavaScript, React, Next Js, SCSS, MUI, Styled
-                components, Tailwind CSS, HTML, RTK, Python, Git.
+                TypeScript, JavaScript, React, Redux, Next Js, (S)CSS, MUI,
+                Styled components, Tailwind CSS, HTML, Markdown, Jest, Python,
+                Git.
               </span>
+            </ul>
+          </li>
+          <li className="mt-4 text-zinc-400 list-disc ml-4">
+            <span className="border-b">Tools:</span>
+            <ul className="list-disc mt-4">
+              <span className="mt-4">Figma, GitHub, Slack, Jira, Trello.</span>
             </ul>
           </li>
           <li className="ml-4 list-disc mt-4 text-zinc-400">
             <span className="border-b">Industry knowledge:</span>
             <ul className="list-disc mt-4">
               <span className="mt-4">
-                Data structures abd algorithm analysis, Agile methodology
+                SEO, Data structures abd algorithm analysis, Agile methodology
               </span>
             </ul>
           </li>
